d67f6e9e305b1797e75cc3fb8c81779b3f3e6ac4
Shahidur Rahman <166603336+srshoruv@users.noreply.github.com>
2025-02-21 12:45:25 +0600

    solved beecrowd problems

--- rotateArrayRight.c
+++ rotateArrayRight.c
@@ -1,20 +1,24 @@
+// rotating Array Right By K Position
+
 #include <stdio.h>
 
 int main()
 {
     int arr[] = {1,2,3,4,5};
     int n = 5;
-    int k = 1;
+    int k;
+    printf("Enter Kth Position: ");
+    scanf("%d", &k);
 
-    // 1 2 3 4 5
-    // 4 5 1 2 3
-    for (int i = k+1; i < n; i++)
+    for (int i = n-k; i < n; i++)
     {
         printf("%d ",arr[i]);
     }
-    for (int i = 0; i <= k; i++)
+    for (int i = 0; i < n-k; i++)
     {
         printf("%d ",arr[i]);
     }
     printf("\n");
+
+    return 0;
 }
